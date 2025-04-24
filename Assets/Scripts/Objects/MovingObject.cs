148488361578d40decdeb35e4d40b18a925f7d15
o.o <6whiteowl6@gmail.com>
2025-04-24 16:04:50 +0900

    [Update] 움직이는 오브젝트 반환 로직 수정
    - 움직이는 오브젝트 10초 후에 풀에 반환하는 코루틴 => Wall에 트리거되면 반환하도록 변경
    - 플레이어 좌우 이동 범위 제한하는 Wall 게임오브젝트 추가

--- Assets/Scripts/Objects/MovingObject.cs
+++ Assets/Scripts/Objects/MovingObject.cs
@@ -21,13 +21,29 @@ public class MovingObject : MonoBehaviour
     {
         if (!GameManager.Instance.IsPlaying) return;    // 이미 게임오버 된 상황에서는 실행 X
         
-        if (other.tag == "Player")
+        if (other.tag == "Player")  // 플레이어와 트리거되면
         {
-            Debug.Log("Player entered");
+            //Debug.Log("Player entered");
             Animator anim = other.gameObject.GetComponentInChildren<Animator>();
-            anim.SetBool("IsDead", true);
+            anim.SetBool("IsDead", true);   // 죽는 애니메이션 재생
             SoundManager.Instance.PlayCollsionSFX();    // 충돌 효과음 재생
             GameManager.Instance.GameOver();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 좌 -> 우 이동, 오른쪽 벽에 트리거되고, 활성화된 상태면
+        if (_direction < 0 && other.CompareTag("RightWall") && isActiveAndEnabled)
+        {
+            PlaySceneManager.Instance.RemoveActiveList(this);
+            PoolManager.Instance.ReturnToPool(name, this);
+        }
+        // 우 -> 좌 이동, 왼쪽 벽에 트리거되고, 활성화된 상태면
+        if (_direction > 0 && other.CompareTag("LeftWall") && isActiveAndEnabled)
+        {
+            PlaySceneManager.Instance.RemoveActiveList(this);
+            PoolManager.Instance.ReturnToPool(name, this);
+        }   
     }
 }
--- Assets/Scripts/Objects/MovingObjectSpawner.cs
+++ Assets/Scripts/Objects/MovingObjectSpawner.cs
@@ -10,13 +10,10 @@ public class MovingObjectSpawner : MonoBehaviour
 
     private void OnEnable()
     {
-        _spawnPoint = transform.GetChild(0);
-        
-        // 스폰 위치 오브젝트 이름으로 방향 확인
-        _isRight = _spawnPoint.gameObject.name == "RightSpawnPoint";
+        _spawnPoint = transform.GetChild(0);    // 스폰 위치 가져오기
+        _isRight = _spawnPoint.gameObject.name == "RightSpawnPoint";    // 스폰 위치 오브젝트 이름으로 방향 확인
 
-        // 스폰 코루틴 실행
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnRoutine()); // 스폰 코루틴 실행
     }
 
     private IEnumerator SpawnRoutine()
@@ -25,31 +22,21 @@ public class MovingObjectSpawner : MonoBehaviour
         {
             // 움직이는 오브젝트 데이터 리스트에서 랜덤으로 하나의 정보 가져오기
             MovingSO movingSO = movingObjectDatas[Random.Range(0, movingObjectDatas.Count)];
-            yield return new WaitForSeconds(movingSO.spawnInterval);
+            yield return new WaitForSeconds(movingSO.spawnInterval); // 스폰 간격만큼 대기
 
             if (Random.value <= movingSO.spawnProbability)
             {
-                SpawnObject(movingSO);
+                SpawnObject(movingSO);  // 스폰 확률에 따라 오브젝트 스폰
             }
         }
     }
 
     private void SpawnObject(MovingSO movingSO)
     {
-        // 이동할 방향 설정
-        float moveDir = _isRight ? -1.0f : 1.0f;
+        float moveDir = _isRight ? -1.0f : 1.0f; // 이동할 방향 설정
         
         MovingObject movingObject = PoolManager.Instance.SpawnFromPool<MovingObject>(movingSO.tag, _spawnPoint.position, Quaternion.identity);
         movingObject.Initialize(moveDir, movingSO.speed);
         PlaySceneManager.Instance.AddActiveList(movingObject);
-        
-        StartCoroutine(ReturnToPoolAfterDelay(movingObject, 10.0f, movingSO.tag));
-    }
-
-    private IEnumerator ReturnToPoolAfterDelay(MovingObject obj, float delay, string poolTag)
-    {
-        yield return new WaitForSeconds(delay);
-        PlaySceneManager.Instance.RemoveActiveList(obj);
-        PoolManager.Instance.ReturnToPool(poolTag, obj);
     }
 }
